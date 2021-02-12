6017df3181e1b122e830ba27c0d8e40cac69437c
opbl11 <opbl11@pcbl1106.cells.es>
2021-02-12 09:24:52 +0100

    Tested on Linux

--- es.alba.sweet.communication/src/es/alba/sweet/communication/command/Command.java
+++ es.alba.sweet.communication/src/es/alba/sweet/communication/command/Command.java
@@ -2,13 +2,19 @@ package es.alba.sweet.communication.command;
 
 public enum Command {
 
-	NAME, STOP, INFO, DEBUG, UNKNOWN;
+	NAME, STOP, EXIT_SERVER, INFO, DEBUG, UNKNOWN;
 
 	public static Command Factory(String name) {
-		if (name.equals(NAME.name())) return NAME;
-		if (name.equals(INFO.name())) return INFO;
-		if (name.equals(DEBUG.name())) return DEBUG;
-		if (name.equals(STOP.name())) return STOP;
+		if (name.equals(NAME.name()))
+			return NAME;
+		if (name.equals(INFO.name()))
+			return INFO;
+		if (name.equals(EXIT_SERVER.name()))
+			return EXIT_SERVER;
+		if (name.equals(DEBUG.name()))
+			return DEBUG;
+		if (name.equals(STOP.name()))
+			return STOP;
 		return UNKNOWN;
 	}
 }
